47e64a2dd74f4aeb0039d2a5d121db1dad419096
bocai <365191806@qq.com>
2023-11-16 19:39:14 +0800

    wwwww

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,4 +1,9 @@
 import request from '../utils/request'
-
+const BaseURl = 'admin/api/manager/article/'
 //获取所有文章
-
+export const findAllPage = (current,size) => {
+    return request({
+      url: BaseURl + `findAllPage/${current}/${size}`,
+      method: 'get'
+    })
+  }
